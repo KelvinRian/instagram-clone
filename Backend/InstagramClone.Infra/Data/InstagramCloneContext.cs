67dd136df98a3be18ca008a7f8a9235afaddf728
KelvinRian <kelvinlourenco18@gmail.com>
2020-10-17 22:17:47 -0300

    adding ef and changing api project version to 3.1

---
+++ Backend/InstagramClone.Infra/Data/InstagramCloneContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InstagramClone.Infra.Data
+{
+    public class InstagramCloneContext : DbContext
+    {
+        public InstagramCloneContext(DbContextOptions<InstagramCloneContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("DefaultConnection");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
